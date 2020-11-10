454b39219e6abb907ce2f759ec55c779df4d73b1
gabriel vasquez <gabriel77780@hotmail.com>
2020-11-09 22:58:32 -0600

    Adding bootstrap and login UI test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,19 +68,19 @@ body {
     </head>
     <body class="text-center">
     <form class="form-signin">
-  <img class="mb-4" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-  <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-  <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+  <img class="mb-4" src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png" alt="" width="72" height="72">
+  <h1 class="h3 mb-3 font-weight-normal">Inicio de Sesión</h1>
+  <label for="inputEmail" class="sr-only">Correo Electrónico</label>
+  <input type="email" id="inputEmail" class="form-control" placeholder="Correo Electrónico" required="" autofocus="">
+  <label for="inputPassword" class="sr-only">Contraseña</label>
+  <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required="">
   <div class="checkbox mb-3">
     <label>
-      <input type="checkbox" value="remember-me"> Remember me
+      <input type="checkbox" value="remember-me"> Recordar Sesión
     </label>
   </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted">© 2017-2020</p>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Ingresar</button>
+  <p class="mt-5 mb-3 text-muted">© 2020-2020</p>
 </form>
 
 
