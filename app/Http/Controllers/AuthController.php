415f3e1900561e8b580ae2295de7fa997b9b629a
gabriel vasquez <gabriel77780@hotmail.com>
2020-11-14 23:08:49 -0600

    Renaming LoginController to AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    protected $authService;
+
+    function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function getLoginView()
+    {
+        return view('login');
+    }
+
+    public function logIn(Request $request)
+    {
+
+        $loggedIn =
+            $this->authService->logIn(
+                $request->input('email'),
+                $request->input('password')
+            );
+
+        return $loggedIn ? redirect('/home') : redirect('/');
+    }
+
+    public function logOut(Request $request)
+    {
+        $this->authService->logOut();
+        return redirect('/');
+    }
+}
