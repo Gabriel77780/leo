f80a9972726aeb9daf55742996aa586197b18da1
gabriel77780 <gabriel77780@hotmail.com>
2020-11-22 01:56:03 -0600

    Adding new model classes

---
+++ database/factories/PermissionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Permission;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PermissionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Permission::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'description' => 'Ingresar paciente',
+            'path' => '/patient',
+            'icon_class' => 'fas fa-space-shuttle',
+            'enabled' => true,
+        ];
+    }
+}
---
+++ database/migrations/2020_11_22_031644_create_role_permissions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_permission', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('permission_id');
+            $table->timestamps();
+        });
+
+        Schema::table('role_permission', function (Blueprint $table) {
+            $table->foreign('role_id')->references('id')->on('role');
+            $table->foreign('permission_id')->references('id')->on('permission');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_permissions');
+    }
+}
