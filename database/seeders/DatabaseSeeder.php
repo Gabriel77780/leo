0a3f2fb17a58c5a05b76ca010e877689616f273a
gabriel77780 <gabriel77780@hotmail.com>
2020-12-10 23:14:58 -0600

    Adding schedule module

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Permission::factory(1)->create();
+        \App\Models\Schedule::factory(1)->create();
     }
 }
